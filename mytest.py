82dd44d6d07a37d193a8ebb8103b89a0460c54fe
olnayoung <39485028+olnayoung@users.noreply.github.com>
2018-05-28 21:28:01 +0900

    test
    no network

--- mytest.py
+++ mytest.py
@@ -11,58 +11,6 @@ import math
 import copy
 import common
 
-def make_model(args, parent=False):
-    return Net()
-
-class Net(nn.Module):
-    def __init__(self, conv=common.default_conv):
-        super(Net, self).__init__()
-
-        pos_head = [conv(1, 128, 7)]
-        pos_body = [common.ResBlock(conv, 128, 3, res_scale=1) for _ in range(5)]
-        pos_tail = [conv(128, 4, 3), nn.Tanh()]
-
-        self.pos_head = nn.Sequential(*pos_head)
-        self.pos_body = nn.Sequential(*pos_body)
-        self.pos_tail = nn.Sequential(*pos_tail)
-
-        def block(in_channel, out_channel):
-            return nn.Sequential(
-                nn.Conv2d(in_channels=in_channel, out_channels=in_channel, kernel_size=3, padding=1),
-                nn.ReLU(inplace=False),
-                nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=3, padding=1),
-                nn.ReLU(inplace=False),
-                nn.AvgPool2d(kernel_size=2, stride=2)
-            )
-
-
-        offset_head = [conv(2, 256, 7)]
-        offset_body = [block(256, 128),
-                       block(128, 64),
-                       block(64, 32),
-                       block(32, 16),
-                       block(16, 8),
-                       block(8, 4)]
-
-        self.offset_head = nn.Sequential(*offset_head)
-        self.offset_body = nn.Sequential(*offset_body)
-
-
-    def forward(self, x, y):
-
-        pos = self.pos_head(x)
-        pos = self.pos_body(pos)
-        pos = self.pos_tail(pos)
-
-        xy = torch.cat([x, y], dim=1)
-        off = self.offset_head(xy)
-        off = self.offset_body(off)
-
-        res4 = pos * off.expand_as(pos)
-        off_1, off_2, off_3, off_4 = torch.split(res4, 1, 1)
-
-        return x + off_1 + off_2 + off_3 + off_4, off
-
 
 def test(input, label, wid, hei):
     input_re = np.zeros((int(hei), int(wid)))
